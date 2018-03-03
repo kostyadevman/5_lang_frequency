ffdb54900ad278364f8c8904b6330c7cf3f86cbc
dolly <timohakt@yandex.ru>
2018-03-03 03:54:24 +0300

    done

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Frequency Analysis of Word allow to now 5 most frequently used words in your text
 
+Example of script launch on Linux, Python 3.5:
+```bash
+$ python lang_frequency.py <path to file>
+
+```
+#Example:
+We have chosen ten of the most famous speeches in English. They range from celebrated, world-changing pieces of rhetoric to our personal favourites, but most importantly they still rouse our emotions when we hear them today. We have examined each for the tricks of the oratory trade. After each speech you’ll find some bullet points outlining its most distinctive rhetorical features, and why a speech writer would include them.
+
+```bash
+$ lang_frequency.py example.txt
+we
+most
+of
+the
+them
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,33 @@
+import os
+import re
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as infile:
+       input_text = infile.read().lower()
+    return input_text
 
+def get_most_frequent_words(input_text):
+    words = re.split('\W+', input_text)
+    words_count = len(words)
+    freq_words = {}
 
-def get_most_frequent_words(text):
-    pass
+    for word in words:
+        if word not in freq_words:
+            freq_words[word] = words.count(word)
 
+    freq_words_sorted = sorted(freq_words, key=freq_words.get, reverse=True)
+
+    return freq_words_sorted
 
 if __name__ == '__main__':
-    pass
+    input_file = sys.argv[1]
+    if os.path.exists(input_file):
+        input_text = load_data(input_file)
+        freq_words_sorted = get_most_frequent_words(input_text)
+        for word in freq_words_sorted[0:5]:
+            print(word)
+
+    else:
+        print("file {} doesn't exists ".format(input_file))
